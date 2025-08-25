ddfa1022bef06928f750c1b17c58f24a7e4b73d6
Devin Conway <devinthomas1208@gmail.com>
2025-08-25 18:31:26 -0400

    add env for "DISCORD_TOKEN" to client.login

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const client = new SapphireClient({
 const main = async () => {
 	try {
 		client.logger.info('Logging in');
-		await client.login();
+		await client.login(process.env.DISCORD_TOKEN);
 		client.logger.info('logged in');
 	} catch (error) {
 		client.logger.fatal(error);
