207954a659bce50e3e815a1230dee8190091bbef
Devin Conway <devinthomas1208@gmail.com>
2025-09-26 22:11:46 -0400

    re-structure server command (for changes about to come)

---
+++ src/commands/admin/server/subcommands/activateSubcommand.ts
@@ -0,0 +1,36 @@
+import { Subcommand } from '@sapphire/plugin-subcommands';
+import { supabase } from '../../../../lib/supabaseClient';
+import { Emojis } from '../../../../utils/emojis';
+import { errorContainer } from '../../../../utils/errorContainer';
+import { MessageContainer } from '../../../../utils/messageContainer';
+
+export async function activateSubcommand(
+  interaction: Subcommand.ChatInputCommandInteraction
+) {
+  const password = interaction.options.getString('password', true);
+
+  const { data, error } = await supabase
+    .from('guilds')
+    .select('id')
+    .eq('activation_password', password)
+    .single();
+
+  if (error || !data) {
+    return errorContainer(interaction, 'Invalid password');
+  }
+
+  const { error: guildUpdateError } = await supabase
+    .from('guilds')
+    .update({ active: true })
+    .eq('id', data.id);
+
+  if (guildUpdateError) {
+    return errorContainer(interaction, guildUpdateError.message);
+  }
+
+  const container = new MessageContainer()
+    .setHeading('Success', Emojis.valid)
+    .setBody('Guild activated');
+
+  return interaction.editReply(container.build('edit'));
+}
